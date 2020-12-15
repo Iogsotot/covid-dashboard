f3c9bf3dda2f41d2dbb12ba84847f423eb8d0f27
Igor Osadchyi <igorosa.com@gmail.com>
2020-12-15 13:39:42 +0200

    feat: map first demo

--- src/components/Main/index.jsx
+++ src/components/Main/index.jsx
@@ -1,6 +1,14 @@
 import React from 'react';
+import Map from '../Map';
 import './index.scss';
 
-const Main = () => <main className="main"><section><h2>Some additional content goes here;)</h2></section></main>;
+const Main = () => (
+  <main className="main">
+    <section>
+      <h2>Some additional content goes here;)</h2>
+    </section>
+    <Map />
+  </main>
+);
 
 export default Main;
