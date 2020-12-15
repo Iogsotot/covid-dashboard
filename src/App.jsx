22aba6726a6f61cc124b3f3e7b53a83df136ae70
Iogsotot <ajustusa@gmail.com>
2020-12-15 23:46:33 +0300

    feat: add console.log for all Total Stats

--- src/App.jsx
+++ src/App.jsx
@@ -9,10 +9,37 @@ import './App.scss';
 class App extends Component {
   async componentDidMount() {
     const stats = new Stats();
-    const covidPerCountriesStats = await stats.getPerCountryStats(true);
+    // const covidPerCountriesStats = await stats.getPerCountryStats(true);
     const covidTotalStats = await stats.getTotalStats(false);
-    console.log(covidPerCountriesStats);
+    // console.log(covidPerCountriesStats);
     console.log(covidTotalStats);
+    // Дата последнего обновления данных:
+    console.log(`Last update: ${(new Date(covidTotalStats.updated)).toDateString()}`);
+    // ~~~~~~~~ данные по всему миру на текущий день ~~~~~~~~~~~
+    // в абсолютных величинах :
+    console.log(`Total cases: ${covidTotalStats.cases}`);
+    console.log(`Total deaths: ${covidTotalStats.deaths}`);
+    console.log(`Total recovered: ${covidTotalStats.recovered}`);
+    // в относительных (на 100 000):
+    console.log(`Total cases Per One Hundred Thousand: 
+    ${Math.round(covidTotalStats.casesPerOneMillion / 10)}`);
+    console.log(`Total deaths Per One Hundred Thousand: 
+    ${Math.round(covidTotalStats.deathsPerOneMillion / 10)}`);
+    console.log(`Total recovered Per One Hundred Thousand: 
+    ${Math.round(covidTotalStats.recoveredPerOneMillion / 10)}`);
+
+    // ~~~~~~~~ данные по всему миру за последний день ~~~~~~~~~~~
+    // в абсолютных величинах :
+    console.log(`Total cases today: ${covidTotalStats.todayCases}`);
+    console.log(`Total deaths today: ${covidTotalStats.todayDeaths}`);
+    console.log(`Total recovered today: ${covidTotalStats.todayRecovered}`);
+    // в относительных (на 100 000):
+    console.log(`Total cases Per One Hundred Thousand today: 
+    ${Math.round((covidTotalStats.todayCases / covidTotalStats.population) * 100000)}`);
+    console.log(`Total deaths Per One Hundred Thousand today: 
+    ${Math.round((covidTotalStats.todayDeaths / covidTotalStats.population) * 100000)}`);
+    console.log(`Total recovered Per One Hundred Thousand today: 
+    ${Math.round((covidTotalStats.todayRecovered / covidTotalStats.population) * 100000)}`);
   }
 
   render() {
