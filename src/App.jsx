ff32d7c43e30cd7cf8487728551705ee696526bc
Iogsotot <ajustusa@gmail.com>
2020-12-16 03:28:25 +0300

    feat: add stats for all needs

--- src/App.jsx
+++ src/App.jsx
@@ -9,24 +9,21 @@ import './App.scss';
 class App extends Component {
   async componentDidMount() {
     const stats = new Stats();
-    // const covidPerCountriesStats = await stats.getPerCountryStats(true);
-    const covidTotalStats = await stats.getTotalStats(false);
-    // console.log(covidPerCountriesStats);
-    console.log(covidTotalStats);
+    const covidTotalStats = await stats.getTotalStats();
+    // сюда передаём название выбранной пользователь страны
+    const inputValue = 'Sweden';
+    const covidPerCountriesStats = await stats.getPerCountryStats(inputValue);
     // Дата последнего обновления данных:
-    console.log(`Last update: ${(new Date(covidTotalStats.updated)).toDateString()}`);
+    console.log(`Last update: ${covidTotalStats.updated}`);
     // ~~~~~~~~ данные по всему миру на текущий день ~~~~~~~~~~~
     // в абсолютных величинах :
     console.log(`Total cases: ${covidTotalStats.cases}`);
     console.log(`Total deaths: ${covidTotalStats.deaths}`);
     console.log(`Total recovered: ${covidTotalStats.recovered}`);
     // в относительных (на 100 000):
-    console.log(`Total cases Per One Hundred Thousand: 
-    ${Math.round(covidTotalStats.casesPerOneMillion / 10)}`);
-    console.log(`Total deaths Per One Hundred Thousand: 
-    ${Math.round(covidTotalStats.deathsPerOneMillion / 10)}`);
-    console.log(`Total recovered Per One Hundred Thousand: 
-    ${Math.round(covidTotalStats.recoveredPerOneMillion / 10)}`);
+    console.log(`Total cases Per One Hundred Thousand: ${covidTotalStats.casesPerOneHundredThousand}`);
+    console.log(`Total deaths Per One Hundred Thousand: ${covidTotalStats.deathsPerOneHundredThousand}`);
+    console.log(`Total recovered Per One Hundred Thousand: ${covidTotalStats.recoveredPerOneHundredThousand}`);
 
     // ~~~~~~~~ данные по всему миру за последний день ~~~~~~~~~~~
     // в абсолютных величинах :
@@ -34,12 +31,33 @@ class App extends Component {
     console.log(`Total deaths today: ${covidTotalStats.todayDeaths}`);
     console.log(`Total recovered today: ${covidTotalStats.todayRecovered}`);
     // в относительных (на 100 000):
-    console.log(`Total cases Per One Hundred Thousand today: 
-    ${Math.round((covidTotalStats.todayCases / covidTotalStats.population) * 100000)}`);
-    console.log(`Total deaths Per One Hundred Thousand today: 
-    ${Math.round((covidTotalStats.todayDeaths / covidTotalStats.population) * 100000)}`);
-    console.log(`Total recovered Per One Hundred Thousand today: 
-    ${Math.round((covidTotalStats.todayRecovered / covidTotalStats.population) * 100000)}`);
+    console.log(`Total cases Per One Hundred Thousand today: ${covidTotalStats.todayCasesPerOneHundredThousand}`);
+    console.log(`Total deaths Per One Hundred Thousand today: ${covidTotalStats.todayDeathsPerOneHundredThousand}`);
+    console.log(`Total recovered Per One Hundred Thousand today: ${covidTotalStats.todayRecoveredPerOneHundredThousand}`);
+
+    // ~~~~~~~~ данные по СТРАНАМ ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+    console.log('~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~');
+    // флаг конкретной страны:
+    console.log(covidPerCountriesStats.flag);
+    // ~~~~~~~~ данные по конкретной стране на текущий день ~~~~~~~~~~~
+    // в абсолютных величинах :
+    console.log(`cases: ${covidPerCountriesStats.cases}`);
+    console.log(`deaths: ${covidPerCountriesStats.deaths}`);
+    console.log(`recovered: ${covidPerCountriesStats.recovered}`);
+    // в относительных (на 100 000):
+    console.log(`cases Per One Hundred Thousand: ${covidPerCountriesStats.casesPerOneHundredThousand}`);
+    console.log(`deaths Per One Hundred Thousand: ${covidPerCountriesStats.deathsPerOneHundredThousand}`);
+    console.log(`recovered Per One Hundred Thousand: ${covidPerCountriesStats.recoveredPerOneHundredThousand}`);
+
+    // ~~~~~~~~ данные по конкретной стране за последний день ~~~~~~~~~~~
+    // в абсолютных величинах :
+    console.log(`cases today: ${covidPerCountriesStats.todayCases}`);
+    console.log(`deaths today: ${covidPerCountriesStats.todayDeaths}`);
+    console.log(`recovered today: ${covidPerCountriesStats.todayRecovered}`);
+    // в относительных (на 100 000):
+    console.log(`cases Per One Hundred Thousand today: ${covidPerCountriesStats.todayCasesPerOneHundredThousand}`);
+    console.log(`deaths Per One Hundred Thousand today: ${covidPerCountriesStats.todayDeathsPerOneHundredThousand}`);
+    console.log(`recovered Per One Hundred Thousand today: ${covidPerCountriesStats.todayRecoveredPerOneHundredThousand}`);
   }
 
   render() {
--- src/components/Stats.jsx
+++ src/components/Stats.jsx
@@ -26,14 +26,65 @@ export default class Stats {
     return covidData;
   }
 
-  async getPerCountryStats(forYesterday = false) {
-    let url = this.urls.perCountryData;
-    if (forYesterday) {
-      url += '?yesterday';
-    }
+  async getPerCountryStats(countryName = 'Uganda') {
+    const url = this.urls.perCountryData;
     const covidData = await this.getDataFromUrl(url);
-    // некие преобразования
-    return covidData;
+    const countries = ['Afghanistan', 'Albania', 'Algeria', 'Andorra', 'Angola', 'Anguilla',
+      'Antigua and Barbuda', 'Argentina', 'Armenia', 'Aruba', 'Australia', 'Austria',
+      'Azerbaijan', 'Bahamas', 'Bahrain', 'Bangladesh', 'Barbados', 'Belarus', 'Belgium',
+      'Belize', 'Benin', 'Bermuda', 'Bhutan', 'Bolivia', 'Bosnia', 'Botswana', 'Brazil',
+      'British Virgin Islands', 'Brunei', 'Bulgaria', 'Burkina Faso', 'Burundi', 'Cabo Verde',
+      'Cambodia', 'Cameroon', 'Canada', 'Caribbean Netherlands', 'Cayman Islands',
+      'Central African Republic', 'Chad', 'Channel Islands', 'Chile', 'China', 'Colombia',
+      'Comoros', 'Congo', 'Costa Rica', 'Croatia', 'Cuba', 'Curaçao', 'Cyprus', 'Czechia',
+      "Côte d'Ivoire", 'DRC', 'Denmark', 'Diamond Princess', 'Djibouti', 'Dominica',
+      'Dominican Republic', 'Ecuador', 'Egypt', 'El Salvador', 'Equatorial Guinea', 'Eritrea',
+      'Estonia', 'Ethiopia', 'Falkland Islands (Malvinas)', 'Faroe Islands', 'Fiji', 'Finland',
+      'France', 'French Guiana', 'French Polynesia', 'Gabon', 'Gambia', 'Georgia', 'Germany',
+      'Ghana', 'Gibraltar', 'Greece', 'Greenland', 'Grenada', 'Guadeloupe', 'Guatemala', 'Guinea',
+      'Guinea-Bissau', 'Guyana', 'Haiti', 'Holy See (Vatican City State)', 'Honduras',
+      'Hong Kong', 'Hungary', 'Iceland', 'India', 'Indonesia', 'Iran', 'Iraq', 'Ireland',
+      'Isle of Man', 'Israel', 'Italy', 'Jamaica', 'Japan', 'Jordan', 'Kazakhstan', 'Kenya',
+      'Kuwait', 'Kyrgyzstan', "Lao People's Democratic Republic", 'Latvia', 'Lebanon',
+      'Lesotho', 'Liberia', 'Libyan Arab Jamahiriya', 'Liechtenstein', 'Lithuania', 'Luxembourg',
+      'MS Zaandam', 'Macao', 'Macedonia', 'Madagascar', 'Malawi', 'Malaysia', 'Maldives', 'Mali',
+      'Malta', 'Marshall Islands', 'Martinique', 'Mauritania', 'Mauritius', 'Mayotte', 'Mexico',
+      'Moldova', 'Monaco', 'Mongolia', 'Montenegro', 'Montserrat', 'Morocco', 'Mozambique',
+      'Myanmar', 'Namibia', 'Nepal', 'Netherlands', 'New Caledonia', 'New Zealand', 'Nicaragua',
+      'Niger', 'Nigeria', 'Norway', 'Oman', 'Pakistan', 'Palestine', 'Panama', 'Papua New Guinea',
+      'Paraguay', 'Peru', 'Philippines', 'Poland', 'Portugal', 'Qatar', 'Romania', 'Russia',
+      'Rwanda', 'Réunion', 'S. Korea', 'Saint Kitts and Nevis', 'Saint Lucia', 'Saint Martin',
+      'Saint Pierre Miquelon', 'Saint Vincent and the Grenadines', 'Samoa', 'San Marino',
+      'Sao Tome and Principe', 'Saudi Arabia', 'Senegal', 'Serbia', 'Seychelles', 'Sierra Leone',
+      'Singapore', 'Sint Maarten', 'Slovakia', 'Slovenia', 'Solomon Islands', 'Somalia',
+      'South Africa', 'South Sudan', 'Spain', 'Sri Lanka', 'St. Barth', 'Sudan', 'Suriname',
+      'Swaziland', 'Sweden', 'Switzerland', 'Syrian Arab Republic', 'Taiwan', 'Tajikistan',
+      'Tanzania', 'Thailand', 'Timor-Leste', 'Togo', 'Trinidad and Tobago', 'Tunisia', 'Turkey',
+      'Turks and Caicos Islands', 'UAE', 'UK', 'USA', 'Uganda', 'Ukraine', 'Uruguay',
+      'Uzbekistan', 'Vanuatu', 'Venezuela', 'Vietnam', 'Wallis and Futuna', 'Western Sahara',
+      'Yemen', 'Zambia', 'Zimbabwe'];
+    const currentCountry = countries.indexOf(countryName);
+    const covidDataPerCountries = {
+      flag: covidData[currentCountry].countryInfo.flag,
+      cases: covidData[currentCountry].cases,
+      deaths: covidData[currentCountry].deaths,
+      recovered: covidData[currentCountry].recovered,
+      casesPerOneHundredThousand: Math.round(covidData[currentCountry].casesPerOneMillion / 10),
+      deathsPerOneHundredThousand: Math.round(covidData[currentCountry].deathsPerOneMillion / 10),
+      recoveredPerOneHundredThousand: Math.round(covidData[currentCountry].recoveredPerOneMillion
+        / 10),
+      todayCases: covidData[currentCountry].todayCases,
+      todayDeaths: covidData[currentCountry].todayDeaths,
+      todayRecovered: covidData[currentCountry].todayRecovered,
+      todayCasesPerOneHundredThousand: ((covidData[currentCountry].todayCases
+        / covidData[currentCountry].population) * 100000).toFixed(3),
+      todayDeathsPerOneHundredThousand: ((covidData[currentCountry].todayDeaths
+        / covidData[currentCountry].population) * 100000).toFixed(3),
+      todayRecoveredPerOneHundredThousand: ((covidData[currentCountry].todayRecovered
+        / covidData[currentCountry].population) * 100000).toFixed(3),
+      updated: (new Date(covidData[currentCountry].updated)).toDateString(),
+    };
+    return covidDataPerCountries;
   }
 
   async getTotalStats(forYesterday = false) {
@@ -42,7 +93,24 @@ export default class Stats {
       url += '?yesterday';
     }
     const covidData = await this.getDataFromUrl(url);
-    // некие преобразования
-    return covidData;
+    const TotalCovidData = {
+      cases: covidData.cases,
+      deaths: covidData.deaths,
+      recovered: covidData.recovered,
+      casesPerOneHundredThousand: Math.round(covidData.casesPerOneMillion / 10),
+      deathsPerOneHundredThousand: Math.round(covidData.deathsPerOneMillion / 10),
+      recoveredPerOneHundredThousand: Math.round(covidData.recoveredPerOneMillion / 10),
+      todayCases: covidData.todayCases,
+      todayDeaths: covidData.todayDeaths,
+      todayRecovered: covidData.todayRecovered,
+      todayCasesPerOneHundredThousand: Math.round((covidData.todayCases
+        / covidData.population) * 100000),
+      todayDeathsPerOneHundredThousand: Math.round((covidData.todayDeaths
+        / covidData.population) * 100000),
+      todayRecoveredPerOneHundredThousand: Math.round((covidData.todayRecovered
+        / covidData.population) * 100000),
+      updated: (new Date(covidData.updated)).toDateString(),
+    };
+    return TotalCovidData;
   }
 }
